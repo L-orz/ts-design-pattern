f815e6007ece5efb4b3587a3aa96484dd1397852
L-orz <22721077+L-orz@users.noreply.github.com>
2021-12-17 17:45:46 +0800

    chore(prettier): add prettier config

--- classic23/state-pattern/index.ts
+++ classic23/state-pattern/index.ts
@@ -1,6 +1,6 @@
 /**
  * 状态模式
- * 
+ *
  * 与策略模式非常相似，最大区别在于，状态模式会将环境(context)保存在状态(state)内，
  * state 在执行完某个动作后，可以直接通过保存的 context 修改当前状态
  * 从而实现了内部状态流转
@@ -16,7 +16,7 @@ class Context {
     this.state = _state
     this.state.setContext(this)
   }
-  
+
   work() {
     this.state.handleWork()
   }
@@ -28,7 +28,7 @@ class Context {
 
 abstract class State {
   context: Context
-  
+
   setContext(_context: Context) {
     this.context = _context
   }
@@ -37,7 +37,7 @@ abstract class State {
   abstract handlePlay()
 }
 
-class HappyState extends State{
+class HappyState extends State {
   handleWork() {
     console.log('Work work work!')
     this.context.transformTo(new SadState())
@@ -57,7 +57,6 @@ class SadState extends State {
   }
 }
 
-
 /**
  * 测试
  */
@@ -66,7 +65,7 @@ function test() {
   context.work()
   context.work()
   context.work()
-  
+
   context.play()
   context.play()
   context.play()
